437521ab36d323b57995f2d182a4d808309955a8
pallavikalshetti <kannur.pallavi@outlook.com>
2025-09-18 11:47:45 -0400

    updating route

--- Pro-Tasker-Frontend/src/App.jsx
+++ Pro-Tasker-Frontend/src/App.jsx
@@ -7,28 +7,28 @@ import ProjectDetails from "./pages/ProjectDetails";
 import AddProject from "./pages/AddProject";
 import AddTask from "./pages/AddTask";
 import EditProject from "./pages/EditPoject";
-
 import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
-
+import EditTask from "./pages/EditTask";
 
 export default function App() {
-return (
-<>
-<Navbar />
-<div className="app">
-<Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
-    <Route path="/projects/:id" element={<ProtectedRoute><ProjectDetails /></ProtectedRoute>} />
-    <Route path="/projects" element={<ProtectedRoute><AddProject /></ProtectedRoute>} />
-    <Route path="/projects/:id/tasks" element={<ProtectedRoute><AddTask /></ProtectedRoute>} />
-    <Route path="/projects/edit/:id" element={<ProtectedRoute><EditProject /></ProtectedRoute>} />
-    <Route path="*" element={<NotFound />} />
-</Routes>
-</div>
-</>
-);
+    return (
+        <>
+        <Navbar />
+            <div className="app">
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
+                    <Route path="/projects/:id" element={<ProtectedRoute><ProjectDetails /></ProtectedRoute>} />
+                    <Route path="/projects" element={<ProtectedRoute><AddProject /></ProtectedRoute>} />
+                    <Route path="/projects/:id/tasks" element={<ProtectedRoute><AddTask /></ProtectedRoute>} />
+                    <Route path="/tasks/edit/:id" element={<ProtectedRoute><EditTask /></ProtectedRoute>} />
+                    <Route path="/projects/edit/:id" element={<ProtectedRoute><EditProject /></ProtectedRoute>} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </div>
+        </>
+    );
 }
