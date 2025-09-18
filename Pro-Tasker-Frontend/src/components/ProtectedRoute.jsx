fc072d892e1628f1969fc673bf99ec7abdc9f858
pallavikalshetti <kannur.pallavi@outlook.com>
2025-09-18 08:19:36 -0400

    updating the route

--- Pro-Tasker-Frontend/src/components/ProtectedRoute.jsx
+++ Pro-Tasker-Frontend/src/components/ProtectedRoute.jsx
@@ -3,12 +3,8 @@ import { useAuth } from "../context/AuthContext";
 
 
 export default function ProtectedRoute({ children }) {
-const { user, loading } = useAuth();
-
-
-if (loading) return <p>Loading...</p>;
-if (!user) return <Navigate to="/" replace />;
-
-
-return children;
+    const { user, loading } = useAuth();
+    if (loading) return <p>Loading...</p>;
+    if (!user) return <Navigate to="/" replace />;
+    return children;
 }
