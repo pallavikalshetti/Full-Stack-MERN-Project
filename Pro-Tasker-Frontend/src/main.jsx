a8afa561c806909ff6523fd6972aa3b1a137141c
pallavikalshetti <kannur.pallavi@outlook.com>
2025-09-18 11:48:19 -0400

    updating main file

--- Pro-Tasker-Frontend/src/main.jsx
+++ Pro-Tasker-Frontend/src/main.jsx
@@ -7,11 +7,11 @@ import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-<BrowserRouter>
-<AuthProvider>
-<App />
-</AuthProvider>
-</BrowserRouter>
-</React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
