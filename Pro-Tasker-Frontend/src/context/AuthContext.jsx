62f77071614f1d3b81dd64b15b7b6b348fc9827a
pallavikalshetti <kannur.pallavi@outlook.com>
2025-09-18 11:49:11 -0400

    updating AuthContext file

--- Pro-Tasker-Frontend/src/context/AuthContext.jsx
+++ Pro-Tasker-Frontend/src/context/AuthContext.jsx
@@ -1,33 +1,30 @@
 import { createContext, useState, useEffect, useContext } from "react";
-
 const AuthContext = createContext();
-
 export function AuthProvider({ children }) {
-const [user, setUser] = useState(null);
-const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-const storedUser = localStorage.getItem("user");
-if (storedUser) setUser(JSON.parse(storedUser));
-setLoading(false);
-}, []);
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) setUser(JSON.parse(storedUser));
+        setLoading(false);
+    }, []);
 
-const login = (userData) => {
-setUser(userData);
-localStorage.setItem("user", JSON.stringify(userData));
-};
+    const login = (userData) => {
+        setUser(userData);
+        localStorage.setItem("user", JSON.stringify(userData));
+    };
 
-const logout = () => {
-setUser(null);
-localStorage.removeItem("user");
-};
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("user");
+    };
 
-return (
-<AuthContext.Provider value={{ user, login, logout, loading }}>
-{children}
-</AuthContext.Provider>
-);
+    return (
+        <AuthContext.Provider value={{ user, login, logout, loading }}>
+        {children}
+        </AuthContext.Provider>
+    );
 }
-
 
 export const useAuth = () => useContext(AuthContext);
